f72caf1df91ff6727845237b1094dd405fa70a03
shubham2441 <40729891+shubham2441@users.noreply.github.com>
2020-03-22 22:42:19 +0530

    Add files via upload

---
+++ Java Basic  Programming/Count the Digits in the Number/digits.java
@@ -0,0 +1,16 @@
+import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    
+    int n = scn.nextInt();
+    int count = 0;
+    while(n!=0){
+        n = n/10;
+        count = count+1;
+    }
+    System.out.println(count);
+   }
+  }
